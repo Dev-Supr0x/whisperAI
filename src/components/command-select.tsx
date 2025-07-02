9f57875bb43928023efd3bff7446f7576f9f5708
Dev-Supr0x <24f3005164@ds.study.iitm.ac.in>
2025-07-02 07:23:54 +0530

    19: meetings filters

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -36,6 +36,11 @@ export const CommandSelect = ({
   const [open, setOpen] = useState(false);
   const selectedOption =options.find((option) => option.value === value);
 
+  const handleOpenChange = (open: boolean) => {
+    onsearch?.("");
+    setOpen(open);
+  };
+
   return (
     <>
       <Button
@@ -56,7 +61,7 @@ export const CommandSelect = ({
       <CommandResponsiveDialog
         shouldFilter={!onsearch}
         open={open}
-        onOpenChange={setOpen}
+        onOpenChange={handleOpenChange}
       >
          <CommandInput placeholder="Search..." onValueChange={onsearch} />
          <CommandList>
